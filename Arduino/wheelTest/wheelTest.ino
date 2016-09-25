07c97d2ffa5089775c47ec403d4d76c313ec4489
jntntnt <jntntnt@users.noreply.github.com>
2016-09-25 02:51:57 -0400

    wheel testing

---
+++ Arduino/wheelTest/wheelTest.ino
@@ -0,0 +1,27 @@
+#include <L298N.h>
+#include <PololuWheelEncoders.h>
+
+int pinLeft1 = 4; // yellow wires
+int pinLeft2 = 5; // yellow wires
+int pinRight1 = 2; // green wires
+int pinRight2 = 3; // green wires
+
+L298N wheels(pinLeft1, pinLeft2, pinRight1, pinRight2, 11, 11, 255);
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  wheels.moveForward();
+  delay(2000);
+  wheels.moveBackward();
+  delay(2000);
+  wheels.rotateRight();
+  delay(2000);
+  wheels.rotateLeft();
+  delay(2000);
+  wheels.stop();
+  delay(2000);
+}
